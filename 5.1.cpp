746d1af12208d41872ded6244c0652c6d10e9e3f
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-29 09:26:33 +0400

    Create 5.1.cpp

---
+++ 5.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+#include "Mod_int.h"
+
+using namespace std;
+
+void Mod_int::add(int i)
+{
+	v += i % 60;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int num;
+	cout << "Число: ";
+	cin >> num;
+	Mod_int int_(num);
+	cout << "Какое число добавить по модулю 60: ";
+	cin >> num;
+	int_.add(num);
+	cout << "Результат: ";
+	int_.print();
+}
