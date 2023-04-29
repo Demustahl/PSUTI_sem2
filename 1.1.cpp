97477c8950fb19a27ecae40e89e0a6d2ebdbc19c
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-29 09:25:58 +0400

    Create 1.1.cpp

---
+++ 1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 97; i <= 122; i++) {
+        cout << char(i) << " ";
+    }
+    for (int i = 65; i <= 90; i++) {
+        cout << char(i) << " ";
+    }
+    for (int i = 160; i <= 175; i++) {
+        cout << char(i) << " ";
+    }
+    for (int i = 224; i <= 239; i++) {
+        cout << char(i) << " ";
+    }
+    for (int i = 128; i <= 159; i++) {
+        cout << char(i) << " ";
+    }
+}
