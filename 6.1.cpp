cb871e43732c519d5848c09f62f0f967c73245e1
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-29 09:26:36 +0400

    Create 6.1.cpp

---
+++ 6.1.cpp
@@ -0,0 +1,27 @@
+﻿#include "header.h"
+#include "rational.h"
+
+inline void comparison(bool v)
+{
+	if (v)
+		cout << "Верно" << endl;
+	else
+		cout << "Неверно" << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	Rational r1(25, 15), r2(35), r3(5.6);
+
+	comparison(r1 == r3);
+	comparison(r1 != r2);
+	comparison(r2 == 35);
+	comparison(r3 >= 5.6);
+	comparison(r1 < r3);
+	comparison(r2 > r3);
+	comparison(r1 <= 0.5);
+
+	return 0;
+}
