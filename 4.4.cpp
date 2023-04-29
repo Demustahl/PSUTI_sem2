a276f051dac65a4ba4e9ee650b06c89ba1e87f71
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-29 09:26:32 +0400

    Create 4.4.cpp

---
+++ 4.4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <locale.h>
+#include "Ch_stack.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    char str[Deque::max_len];
+    cout << "Введите строку которую хотите обратить:" << endl;
+    cin.get(str, Deque::max_len);
+    Ch_stack ch_stack(str);
+    cout << "Результат:" << endl;
+    cout << ch_stack.getSwapStr() << endl;
+}
