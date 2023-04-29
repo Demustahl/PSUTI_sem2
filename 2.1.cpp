21ce8b76089ccb34b729267861d6eda47edbeb49
Максим Конев <111451752+Demustahl@users.noreply.github.com>
2023-04-29 09:26:12 +0400

    Create 2.1.cpp

---
+++ 2.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "");
+    for (int i = 0;i<argc;i++)
+        cout << i << ". " << argv[i] << endl;
+    return 0;
+}
+
+//echo_arf a man a plan a canal panama
